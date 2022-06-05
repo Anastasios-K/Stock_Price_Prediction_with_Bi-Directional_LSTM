8bb5515a6aed734dcd3622a07319345c0c77c0d9
TasKat <anastasios.katsikogiannis@atherasanalytics.com>
2022-06-05 20:50:54 +0100

    nan detection

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from scr.preproc import read_data
+from scr.preproc import read_data, add_nans, detect_nan_values, detect_null_values
 
 
-a = read_data()
+url_list = ["data", "btcusd_datetime.csv"]
+
+df = read_data(url_list=url_list)
+df_nan = add_nans(df=df)
+
+x = detect_nan_values(df_nan)
+y = detect_null_values(df_nan)
