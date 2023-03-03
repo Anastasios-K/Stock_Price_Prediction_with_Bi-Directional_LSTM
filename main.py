d8ae3bd050c0b13e18aebe86bb839eb68fb8948e
TasKat <anastasios.katsikogiannis@atherasanalytics.com>
2023-03-03 17:01:46 +0000

    readme update

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ class RunStockPrediction:
         config = Configurator(config_path=config_path)
         # Create model_builder object
         model_builder = LstmBuilder
-        # Create pipeline object
+        # # Create pipeline object
         self.pipeline = Pipeline(pipeline_config=config, model_builder=model_builder)
-        pipe = self.pipeline
-        # Execute exploration
-        pipe.explore_data().plot_multi_resolution(title="data_overview_multiple_resolution")
-        pipe.explore_data().plot_distribution(fig_title="data_distributions")
-        pipe.explore_data().plot_correlation("correltion_heatmap")
-        pipe.explore_data().plot_autocorrelation()
-        pipe.explore_data().cerate_eda_report(report_name="explanatory_data_analysis_report")
-        # Execute enrichment
-        pipe.enrich_data(sma=False, mfi=True, ema=False, macd=True)
-        # Scale data
-        pipe.scale_data()
-        # Build model
-        pipe.build_model()
-        # Execute training testing and tracking
-        pipe.execute_training_testing_tracking()
+        # pipe = self.pipeline
+        # # Execute exploration
+        # pipe.explore_data().plot_multi_resolution(title="data_overview_multiple_resolution")
+        # pipe.explore_data().plot_distribution(fig_title="data_distributions")
+        # pipe.explore_data().plot_correlation("correltion_heatmap")
+        # pipe.explore_data().plot_autocorrelation()
+        # pipe.explore_data().cerate_eda_report(report_name="explanatory_data_analysis_report")
+        # # Execute enrichment
+        # pipe.enrich_data(sma=False, mfi=True, ema=False, macd=True)
+        # # Scale data
+        # pipe.scale_data()
+        # # Build model
+        # pipe.build_model()
+        # # Execute training testing and tracking
+        # pipe.execute_training_testing_tracking()
 
 
 if __name__ == "__main__":
